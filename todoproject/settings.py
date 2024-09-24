abdb49dedf2a8cbd2c3816d745af8973e48fb2c3
Skywalker185 <yusuke.miyoshi0524@gmail.com>
2024-09-24 20:28:20 +0900

    7回目

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -13,7 +13,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = env('DEBUG')
 
